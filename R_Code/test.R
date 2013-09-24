7462c99ef9bf55903ea75a73c96b02d3d09244c7
unknown <swarnholz@WRZ04641.campus.fu-berlin.de>
2013-09-24 14:23:57 +0200

    example update

--- R_Code/test.R
+++ R_Code/test.R
@@ -3,11 +3,40 @@ if(.Platform$OS.type == "windows") {
   install_github(repo="parallelTools", username = "wahani", subdir = "package")
 }
 require(parallelTools)
-mclapply(1:10, 
-         function(x) {
-           data(oats)
-           summary(oats)
-         }, mc.cores = detectCores(), packageToLoad = c("MASS", "parallel"),
-         sourceFile = "R_Code/MClapply.R")
 
+genData <- function(n) {
+  # Generate some Data
+  dat <- data.frame()
+  for (i in 1:n) {
+    dat <- rbind(dat, data.frame(colA = letters[sample(26)], colB = letters[sample(26)]))
+  }
+  dat
+}
 
+slowFunction <- function(dat) {
+  # A slow function
+  dat$ind <- logical(nrow(dat))
+  
+  for (i in 1:nrow(dat)) {
+    dat$ind[i] <- grepl(dat$colA[i], dat$colB[i])
+  }
+  
+  dat
+}
+
+nCores <- detectCores() - 1 # Detects the number of cores
+dat <- genData(1000) # generate some Date
+dat$sample <- rep_len(1:nCores, length.out = nrow(dat)) # ID for splitting the data
+dat$id <- 1:nrow(dat) # ID for observations
+dataList <- split(dat, dat$sample) # split the dataSet for processing the tasks parallel
+
+system.time(result1 <- slowFunction(dat)) # Function without mc
+
+# Function with mc - should work for all platforms
+system.time(result2 <- do.call("rbind", mclapply(X = dataList, FUN = slowFunction, mc.cores = nCores)))
+
+# Better than mc is vecorization (in this case) - no multicore required:
+system.time(result3 <- mapply(grepl, as.list(dat$colA), as.list(dat$colB)))
+
+#All the same
+all((result1$ind == result2$ind[order(result2$id)]) & result1$ind == result3)
